a29fdc2814ecbc1f53efb2570b90d438b398063a
harsha126 <63415146+harsha126@users.noreply.github.com>
2022-05-01 16:40:31 +0530

    first commmit
    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./index.css";
+import Lab from "./components/Lab";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import "./App.css";
+
+function App() {
+  var username = "USERNAME"
+  var Pass = "PASSWORD"
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
